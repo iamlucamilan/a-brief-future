65b118e2d25d4d660b5da69d9921ffcd4565d127
Luca Milan <milan.luca@gmail.coim>
2020-05-29 10:55:36 +0100

    Add random options based on max id on the database

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,37 @@ export async function getServerSideProps() {
   let firebase = await loadFirebase()
   let db = firebase.firestore()
   var result = {}
-
-  // const document = db.collection('actions').doc()
-  // const documentUuid = document.id
-  // console.log(documentUuid)
+  var maximum_ranges = {}
   
+  /**
+   * ACTIONS
+   */
 
-  // Retrieve action
+  // Set action collection reference
+  let actionsRef = db.collection('actions')
 
-  let actionsRef = db.collection('actions').limit(1)
-  let action = await actionsRef.get() 
+  // Find maximum result for that specific collection
+  let actionMaxRef = actionsRef.orderBy('id', 'desc').limit(1);
+  let actionMax = await actionMaxRef.get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+
+    snapshot.forEach(doc => {
+      maximum_ranges.action = doc.data().id
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+
+  // Random number between 1 and maximum_ranges of that specific collection 
+  let action_id = Math.floor((Math.random() * maximum_ranges.action) + 1);
+
+  // Query the specific document
+  let action = await actionsRef.where('id', '==', action_id).get() 
     .then(snapshot => {
       snapshot.forEach(doc => {
         console.log(doc.data())
@@ -41,11 +62,35 @@ export async function getServerSideProps() {
       console.log('Error getting documents', err);
     });
 
+   /**
+   * OBJECT
+   */
 
-  // Retrieve object
+  // Set action collection reference
+  let objectRef = db.collection('objects')
 
-  let objectsRef = db.collection('objects').limit(1)
-  let object = await objectsRef.get() 
+  // Find maximum result for that specific collection
+  let objectMaxRef = objectRef.orderBy('id', 'desc').limit(1);
+  let objectMax = await objectMaxRef.get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+
+    snapshot.forEach(doc => {
+      maximum_ranges.object = doc.data().id
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+
+  // Random number between 1 and maximum_ranges of that specific collection 
+  let object_id = Math.floor((Math.random() * maximum_ranges.object) + 1);
+
+  // Query the specific document
+  let object = await objectRef.where('id', '==', object_id).get() 
     .then(snapshot => {
       snapshot.forEach(doc => {
         console.log(doc.data())
@@ -56,10 +101,35 @@ export async function getServerSideProps() {
       console.log('Error getting documents', err);
     });
 
-  // Retrieve need
+  /**
+   * NEED
+   */
 
-  let needsRef = db.collection('needs').limit(1);
-  let need = await needsRef.get() 
+  // Set action collection reference
+  let needRef = db.collection('needs')
+
+  // Find maximum result for that specific collection
+  let needMaxRef = needRef.orderBy('id', 'desc').limit(1);
+  let needMax = await needMaxRef.get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+
+    snapshot.forEach(doc => {
+      maximum_ranges.need = doc.data().id
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+
+  // Random number between 1 and maximum_ranges of that specific collection 
+  let need_id = Math.floor((Math.random() * maximum_ranges.need) + 1);
+
+  // Query the specific document
+  let need = await needRef.where('id', '==', need_id).get() 
     .then(snapshot => {
       snapshot.forEach(doc => {
         console.log(doc.data())
@@ -70,10 +140,35 @@ export async function getServerSideProps() {
       console.log('Error getting documents', err);
     });
 
-  // Retrieve world
+  /**
+   * WORLD
+   */
 
-  let worldsRef = db.collection('worlds').limit(1);
-  let world = await worldsRef.get() 
+  // Set action collection reference
+  let worldRef = db.collection('worlds')
+
+  // Find maximum result for that specific collection
+  let worldMaxRef = worldRef.orderBy('id', 'desc').limit(1);
+  let worldMax = await worldMaxRef.get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+
+    snapshot.forEach(doc => {
+      maximum_ranges.world = doc.data().id
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+
+  // Random number between 1 and maximum_ranges of that specific collection 
+  let world_id = Math.floor((Math.random() * maximum_ranges.world) + 1);
+
+  // Query the specific document
+  let world = await worldRef.where('id', '==', world_id).get() 
     .then(snapshot => {
       snapshot.forEach(doc => {
         console.log(doc.data())
@@ -84,10 +179,35 @@ export async function getServerSideProps() {
       console.log('Error getting documents', err);
     });
 
-  // Retrieve premise
-  
-  let premisesRef = db.collection('premises').limit(1);
-  let premise = await premisesRef.get() 
+  /**
+   * PREMISE
+   */
+
+  // Set action collection reference
+  let premiseRef = db.collection('premises')
+
+  // Find maximum result for that specific collection
+  let premiseMaxRef = premiseRef.orderBy('id', 'desc').limit(1);
+  let premiseMax = await premiseMaxRef.get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }  
+
+    snapshot.forEach(doc => {
+      maximum_ranges.premise = doc.data().id
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+
+  // Random number between 1 and maximum_ranges of that specific collection 
+  let premise_id = Math.floor((Math.random() * maximum_ranges.premise) + 1);
+
+  // Query the specific document
+  let premise = await premiseRef.where('id', '==', premise_id).get() 
     .then(snapshot => {
       snapshot.forEach(doc => {
         console.log(doc.data())
@@ -97,6 +217,8 @@ export async function getServerSideProps() {
     .catch(err => {
       console.log('Error getting documents', err);
     });
+
+
 
 
   console.log(result)

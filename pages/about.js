3e4bc69d431d66481b9206590fb7c531775ef769
Luca Milan <milan.luca@gmail.coim>
2020-08-11 18:12:46 +0200

    Fix mailto link

--- pages/about.js
+++ pages/about.js
@@ -21,7 +21,7 @@ function About({  }) {
           We are <a href="https://www.alterneering.com" target="_blank">alterneering studios</a>, a speculative design studio from London. <br/> <span className="highlight">We create ideas in alternative realities.</span> To help us do this, we developed this tool that drops us into a future scenario. <br/> Feel free to use it and help a random citizen with the challenge they are facing.
         </p>
         <p>
-          We enjoy this sort of brain teaser and appreciate it if you do too. <br/>Please contact us for any sort of feedback, unnecessary comments, collaboration requests or serious wedding proposals. <br/>We do <a href="mailto:hq@alterneeringstudios.com&subject=Yo!&body=Hello email sender, we appreciate you. Did you enjoyed the sun lately?">email</a> but prefer Instagram at <a href="https://www.instagram.com/alterneeringstudios" target="_blank">@alterneeringstudios</a>
+          We enjoy this sort of brain teaser and appreciate it if you do too. <br/>Please contact us for any sort of feedback, unnecessary comments, collaboration requests or serious wedding proposals. <br/>We do <a href="mailto:hq@alterneering.com?subject=Yo!&body=Hello%20email%20sender%2C%20we%20appreciate%20you.%20Did%20you%20enjoyed%20the%20sun%20lately%3F" >email</a> but prefer Instagram at <a href="https://www.instagram.com/alterneeringstudios" target="_blank">@alterneeringstudios</a>
         </p>
         <p>
           Have a nice day and keep doubting. <br/>Enjoy the sun! ☉
